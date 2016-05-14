5359e1a453cc6e12ca3daec765422512a6d0250b
townsean <ashley.grenon@gmail.com>
2016-05-13 20:45:30 -0500

    created model for emotion

---
+++ app/model/emotion.js
@@ -0,0 +1,8 @@
+"use strict";
+var Emotion = (function () {
+    function Emotion() {
+    }
+    return Emotion;
+}());
+exports.Emotion = Emotion;
+//# sourceMappingURL=emotion.js.map
---
+++ app/model/emotion.ts
@@ -0,0 +1,7 @@
+export class Emotion {
+    username: string;
+    status: string;
+    emotion: string;
+    avatarUrl: string;
+    date: Date;
+}
