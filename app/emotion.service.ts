56a3d6b3b4c3017d10c1f35cdbcbfb0884fbd894
townsean <ashley.grenon@gmail.com>
2016-05-13 20:02:04 -0500

    Adding a message for Mike

--- app/emotion.service.ts
+++ app/emotion.service.ts
@@ -2,5 +2,6 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class EmotionService {
-     
+     // given a date range, return a collection of emotions
+     // avatar, display name, text analyzed and what it was classified as
 }
