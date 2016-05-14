d0e8e16b2d1fe7f74ff404b412da10aa4872e475
townsean <ashley.grenon@gmail.com>
2016-05-13 19:43:57 -0500

    getting initial setup complete

---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>Moody: The emotional polarity of your social network</h1>'
+})
+export class AppComponent { }
