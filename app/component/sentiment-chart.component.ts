80bdd1a863352c8333bf37eab834bacd7bbd4d08
townsean <ashley.grenon@gmail.com>
2016-05-15 15:59:34 -0500

    chart changes

--- app/component/sentiment-chart.component.ts
+++ app/component/sentiment-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
 import { Sentiment } from '../model/sentiment';
 import { OnActivate } from '@angular/router';
 
@@ -7,26 +7,64 @@ declare var Chart:any;
 @Component({
     selector: 'sentiment-chart',
     template: `
-        <section>
-            <canvas id="chart" width="400" height="200"></canvas>
-        </section>
+        <div>
+            <canvas id="mychart" width="400" height="200"></canvas>
+        </div>
     `
 })
-export class SentimentChartComponent implements OnActivate {    
+export class SentimentChartComponent implements AfterViewInit {    
     @Input()
     sentiments: Sentiment[];
+    context: any;
+    chart: any;
     
-    routerOnActivate() {
-        let chartContext = document.getElementById("chart");
-        let chart = new Chart(chartContext, {
+    constructor() {
+        Chart.defaults.global.responsive = true;
+    }
+    
+    ngAfterViewInit() {
+        let canvas = <HTMLCanvasElement> document.getElementById("mychart");
+        this.context = <CanvasRenderingContext2D> canvas.getContext("2d");
+        // this.chart = new Chart(this.context, {
+        //     type: 'doughnut',
+        //     data: {
+        //         datasets: [{
+        //             data: [5, 10, 15],
+        //             backgroundColor: [ "#FF0000", "#00FF00", "#0000FF"],
+        //             label: 'Sentiments'
+        //         }],
+        //         labels: ["Positive", "Negative", "Neutral"]
+        //     }
+        // });
+        this.chart = new Chart(this.context, {
             type: 'doughnut',
             data: {
-                datasets: [{
-                    data: [5, 10, 15],
-                    backgroundColor: [ "#FF0000", "#00FF00", "#0000FF"],
-                    label: 'Sentiments'
-                }],
-                labels: ["Positive", "Negative", "Neutral"]
+                labels: [
+                    "Red",
+                    "Green",
+                    "Yellow"
+                ],
+                datasets: [
+                    {
+                        data: [300, 50, 100],
+                        backgroundColor: [
+                            "#FF6384",
+                            "#36A2EB",
+                            "#FFCE56"
+                        ],
+                        hoverBackgroundColor: [
+                            "#FF6384",
+                            "#36A2EB",
+                            "#FFCE56"
+                        ]
+                    }]
+            },
+            options: {
+                elements: {
+                    arc: {
+                        borderColor: "#000000"
+                    }
+                }
             }
         });
     }    
